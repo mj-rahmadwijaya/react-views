882681d81e052a38a0ca5e63f282de724677c0b3
mj-rahmadwijaya <rahmad.wijaya@magic-jungle.co.id>
2025-05-01 14:39:05 +0700

    ipc renderer

--- src/pages/ElectronViews.jsx
+++ src/pages/ElectronViews.jsx
@@ -1,21 +1,41 @@
 import React, { useEffect, useState } from 'react';
 
 const ElectronViews = () => {
-    const [productCategoryList, setProductCategoryList] = useState([]); 
-    const [product, setProduct] = useState([]);
-    const [categoryName, setCategoryName] = useState('');
-    const [modal, setModal] = useState(false);
+    // create state management
+    const [data, setData] = useState({
+        productCategoryList: [],
+        product: [],
+        categoryName: '',
+        modal: false,
+        loading: false
+    }); 
+    const { productCategoryList, product, categoryName, modal, loading } = data;
+    const setDataState = (newData) => {
+        setData(prevData => ({ ...prevData, ...newData }));
+    };
 
     const getProductCategoryList = async () => {
+        setDataState({
+            loading: true
+        });
         const products = await window.electronAPI.fetchProductCategoryList();
-        setProductCategoryList(products);
+        setDataState({
+            productCategoryList: products,
+            loading: false
+        });
     };
 
     const getProductByCategory = async (category) => {
-        setCategoryName(category);
+        setDataState({
+            categoryName: category,
+            loading: true
+        });
         const products = await window.electronAPI.fetchProductByCategory(category); 
-        setProduct(products.products);
-        setModal(true);
+        setDataState({
+            product: products.products,
+            modal: true,
+            loading: false
+        });
     };
 
     const modalProduct = () => {
@@ -24,7 +44,7 @@ const ElectronViews = () => {
                 <div className='bg-white p-4 rounded-md absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-3/4 overflow-y-auto'>
                     <div className='p-2 border-b-2 relative'>
                         <h4 className='text-lg font-bold uppercase'>{categoryName}</h4>
-                        <button className='p-1 cursor-pointer rounded-md border-2 border-gray-300 absolute right-1 top-1' onClick={() => setModal(false)}>
+                        <button className='p-1 cursor-pointer rounded-md border-2 border-gray-300 absolute right-1 top-1' onClick={() => setDataState({ modal: false })}>
                             <svg fill="#000000" width="20px" height="20px" viewBox="0 0 24 24" id="cross" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg" className="icon flat-color"><path id="primary" d="M13.41,12l6.3-6.29a1,1,0,1,0-1.42-1.42L12,10.59,5.71,4.29A1,1,0,0,0,4.29,5.71L10.59,12l-6.3,6.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L12,13.41l6.29,6.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42Z"></path></svg>
                         </button>
                     </div>
@@ -55,6 +75,13 @@ const ElectronViews = () => {
                 ))}
             </div>
             {modal && modalProduct()}
+            {loading && <div className='p-6 fixed inset-0 bg-[#00000085] flex items-center justify-center'>
+                <div className='bg-white p-4 rounded-md'>
+                    <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M16 8.00023L18.3642 5.63609M5.63631 18.364L8.00026 16M17.6566 12H21M3 12H6.34315M12 6.34342L12 3M12 21L12 17.6569M8.00023 8.00023L5.63609 5.63609M18.364 18.364L16 16" stroke="#363853" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                </div>
+            </div>}
         </>
     );
 };
--- src/pages/ReactViews.jsx
+++ src/pages/ReactViews.jsx
@@ -2,31 +2,58 @@ import React, { useEffect, useState } from 'react';
 import useHITApi from '../hooks/useHitAPI';
 
 const ReactViews = () => {
-    const [products, setProducts] = useState([]);
-    const [search, setSearch] = useState('');
-    const [modal, setModal] = useState(false);
-    const [modalData, setModalData] = useState(null);
+    const [data, setData] = useState({
+        products: [],
+        search: '',
+        modal: false,
+        modalData: null,
+        loading: false
+    });
+
+    const { products, search, modal, modalData, loading } = data;
+    const setDataState = (newData) => {
+        setData(prevData => ({ ...prevData, ...newData }));
+    };
     const hitAPI = useHITApi();
 
     const getProductsAll = async () => {
+        setDataState({
+            loading: true
+        });
         const x = await hitAPI('https://dummyjson.com/products');
-        setProducts(x.products);
+        setDataState({
+            products: x.products,
+            loading: false
+        });
     };
 
     const handleSearch = (e) => {
-        setSearch(e.target.value);
+        setDataState({
+            search: e.target.value
+        });
     }
 
     const handleSearchClick = async () => {
-        console.log(search);
+        setDataState({
+            loading: true
+        });
         const x = await hitAPI(`https://dummyjson.com/products/search?q=${search}`);
-        setProducts(x.products);
+        setDataState({
+            products: x.products,
+            loading: false
+        });
     }
 
     const clickModal = async (product) => {
+        setDataState({
+            loading: true
+        });
         const x = await hitAPI(`https://dummyjson.com/products/${product.id}`);
-        setModalData(x);
-        setModal(true);
+        setDataState({
+            modalData: x,
+            modal: true,
+            loading: false
+        });
     }
 
     const modalDetail = () => {
@@ -35,7 +62,7 @@ const ReactViews = () => {
                 <div className='bg-white p-4 rounded-md'>
                     <div className='p-2 border-b-2 relative'>
                         <h4>Detail Product</h4>
-                        <button className='p-1 cursor-pointer rounded-md border-2 border-gray-300 absolute right-1 top-1' onClick={() => setModal(false)}>
+                        <button className='p-1 cursor-pointer rounded-md border-2 border-gray-300 absolute right-1 top-1' onClick={() => setDataState({ modal: false })}>
                             <svg fill="#000000" width="20px" height="20px" viewBox="0 0 24 24" id="cross" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg" className="icon flat-color"><path id="primary" d="M13.41,12l6.3-6.29a1,1,0,1,0-1.42-1.42L12,10.59,5.71,4.29A1,1,0,0,0,4.29,5.71L10.59,12l-6.3,6.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L12,13.41l6.29,6.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42Z"></path></svg>
                         </button>
                     </div>
@@ -74,6 +101,14 @@ const ReactViews = () => {
 
             </div>
             {modal && modalDetail(products[0])}
+
+            {loading && <div className='p-6 fixed inset-0 bg-[#00000085] flex items-center justify-center'>
+                <div className='bg-white p-4 rounded-md'>
+                    <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M16 8.00023L18.3642 5.63609M5.63631 18.364L8.00026 16M17.6566 12H21M3 12H6.34315M12 6.34342L12 3M12 21L12 17.6569M8.00023 8.00023L5.63609 5.63609M18.364 18.364L16 16" stroke="#363853" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                </div>
+            </div>}
         </>
     );
 };
